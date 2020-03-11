6875bf810d96fce8703a2927566414b114df1fe5
bryson-davis <qinkai1994@qq.com>
2020-03-12 07:37:26 +0800

    update

--- src/mr/master.go
+++ src/mr/master.go
@@ -7,9 +7,26 @@ import "net/rpc"
 import "net/http"
 
 
+const (
+	SCHEDULE_TASK_SUCCESS = "success"
+	SCHEDULE_TASK_FAILED = "fail" //调度不成功，
+	SCHEDULE_TASK_DONE = "done" //已经全部调度完毕
+)
+
 type Master struct {
 	// Your definitions here.
+	files []string
 
+	//m个map和n个reduce
+	nReduce int
+	mMap int
+
+	//管理字段,通道用于通信/等待
+	mapIndexChan chan int //map 任务下标的通道
+	reduceIndexChan chan int
+	//用于保存正在运行的任务，
+	runningMapTask map[int]int64
+	runningReduceTask map[int]int64
 }
 
 
@@ -20,20 +37,69 @@ func (m *Master) AskForTask(args *AskForTaskArgs, reply *AskForTaskReply) error 
 
 	//进行任务分配
 	for {
-		task := m.scheduleTask()
-		reply.Done = false
-		reply.Task = *task
+		task, result := m.scheduleTask()
 	}
 
 	return nil
 }
 
-func (m *Master) scheduleTask() *Task {
-	return nil
+//分配任务
+func (m *Master) scheduleTask() (*Task, string) {
+	//先分配map task，保证全部的map task都分配出去了并完成了，才进入reduce的环节
+	select {
+	case mapIndex := <-m.mapIndexChan:
+		task := &Task{
+			Phase:      TASK_PHASE_MAP,
+			MapTask:    MapTask{
+				FileName: m.files[mapIndex],
+				MapIndex: mapIndex,
+				ReduceNumber: m.nReduce,
+			},
+		}
+		return task, SCHEDULE_TASK_SUCCESS
+	default:
+		//表示任务已经分配完毕，但可能还有正在运行的任务
+		if len(m.runningReduceTask) > 0 {
+			return nil, SCHEDULE_TASK_FAILED
+		}
+	}
+
+	select {
+	case reduceIndex := <-m.reduceIndexChan:
+		task := &Task{
+			Phase:      TASK_PHASE_REDUCE,
+			ReduceTask: ReduceTask{
+				MapNumber: m.mMap,
+				ReduceIndex: reduceIndex,
+			},
+		}
+		return task, SCHEDULE_TASK_SUCCESS
+	default:
+		if len(m.runningReduceTask) > 0 {
+			return nil, SCHEDULE_TASK_FAILED
+		}
+	}
+
+	return nil, SCHEDULE_TASK_DONE
 }
 
+//处理已经处理成功的任务
 func (m *Master) finishTask(task Task) {
+	switch task.Phase {
+	case TASK_PHASE_MAP:
+		//判断是否在running里面
+		if _, ok := m.runningMapTask[task.MapTask.MapIndex]; !ok {
+			//可能已经被其他worker处理了,因为有超时重试的机制
+			return
+		}
+		delete(m.runningMapTask, task.MapTask.MapIndex)
 
+	case TASK_PHASE_REDUCE:
+		if _, ok := m.runningReduceTask[task.ReduceTask.ReduceIndex]; !ok {
+			return
+		}
+		delete(m.runningReduceTask, task.ReduceTask.ReduceIndex)
+	}
 }
 
 //
@@ -85,7 +151,24 @@ func MakeMaster(files []string, nReduce int) *Master {
 	m := Master{}
 
 	// Your code here.
+	m.files = files
 
+	m.mMap = len(files)
+	m.nReduce = nReduce
+
+	m.mapIndexChan = make(chan int, m.mMap)
+	m.reduceIndexChan = make(chan int, m.nReduce)
+	m.runningMapTask = make(map[int]int64, 0)
+	m.runningReduceTask = make(map[int]int64, 0)
+
+	//启动任务通讯
+	for i := 0; i < m.mMap; i++ {
+		m.mapIndexChan <- i //每个值代表一个任务序号
+	}
+
+	for i := 0; i < m.nReduce; i++ {
+		m.reduceIndexChan <- i
+	}
 
 	m.server()
 	return &m
